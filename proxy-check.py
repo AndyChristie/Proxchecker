47139cf917d8ac6258ed57bb67a12678c91c438c
AndyChristie <andy.christie93@hotmail.co.uk>
2016-01-27 14:03:23 +0100

    Update proxy-check.py

--- proxy-check.py
+++ proxy-check.py
@@ -4,8 +4,9 @@ import urllib2
 import time
 from tqdm import tqdm
 
-input_file = 'proxies_1.txt'
-threads    = 10
+input_file      = 'proxies_.txt'
+working_proxies = 'working_proxies.txt'
+threads         = 30
 
 queue  = Queue.Queue()
 output = []
@@ -15,39 +16,39 @@ class ThreadUrl(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self.queue = queue
-
+    
     def run(self):
         while True:
+            #grabs host from queue
+            proxy_info = self.queue.get()
             for host in tqdm(range(self.queue.qsize())):
-                #grabs host from queue
-                proxy_info = self.queue.get()
-                
                 try:
-                    proxy_handler      = urllib2.ProxyHandler({'http':proxy_info})
-                    opener             = urllib2.build_opener(proxy_handler)
-                    opener.addheaders  = [('User-agent','Mozilla/5.0')]
+                    proxy_handler     = urllib2.ProxyHandler({'http':proxy_info})
+                    opener            = urllib2.build_opener(proxy_handler)
+                    opener.addheaders = [('User-agent','Mozilla/5.0')]
                     urllib2.install_opener(opener)
-                    req                = urllib2.Request("http://www.google.com")
-                    sock               = urllib2.urlopen(req, timeout= 7)
-                    rs                 = sock.read(1000)
+                    req               = urllib2.Request("http://www.google.com")
+                    sock              = urllib2.urlopen(req, timeout= 7)
+                    rs                = sock.read(1000)
                     if '<title>Google</title>' in rs:
-                        output.append(('0',proxy_info))
+                        output.append(('0', proxy_info))
+                        with open(working_proxies, 'a') as working:
+                            working.write(proxy_info+'\n')
                     else:
                         raise "Not Google"
                 except:
                     output.append(('x',proxy_info))
-                #signals to queue job is done
+                # signals to queue job is done
                 self.queue.task_done()
 
 start = time.time()
 def main():
-    
     #spawn a pool of threads, and pass them queue instance 
     for i in range(5):
         t = ThreadUrl(queue)
         t.setDaemon(True)
         t.start()
-    hosts = [host.strip() for host in open(input_file)]
+    hosts = [host.strip() for host in open(input_file).readlines()]
     #populate queue with data   
     for host in hosts:
         queue.put(host)
